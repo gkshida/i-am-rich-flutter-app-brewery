975f945706fd3b9c77f756c0c651dfd0cf00be77
Guilherme Kayo Shida <guilhermeshida@gmail.com>
2019-10-15 14:13:16 -0300

    Scaffolding an Material App
    * Used some Colors;
    * Learned about Scaffold.

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,20 @@ class IAmRichApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        Text('Hello World!')
-      ),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text('I Am Rich'),
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+              'https://images.pexels.com/photos/146080/pexels-photo-146080.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+            ),
+          ),
+        ),
+      )
     );
   }
 }
